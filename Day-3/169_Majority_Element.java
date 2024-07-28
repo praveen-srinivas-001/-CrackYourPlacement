f0f181c0b43f38967e0b5ace0d113b47d4073283
praveen-srinivas-001 <124066261+praveen-srinivas-001@users.noreply.github.com>
2024-07-28 17:56:51 +0530

    Arrays - Counting

---
+++ Day-3/169_Majority_Element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0;
+        int count = 0;
+        for(int num:nums){
+            if(count == 0) res = num;
+            count += (res == num)?1:-1;
+        }
+        return res;
+    }
+} // Boyer-moore Algorithm.
